16a75a8b7741db611ca5a7aa2d9051cdf164b004
inocop <inoue19851110@gmail.com>
2019-02-14 13:56:56 +0900

    fix docker env

---
+++ src/docker/vue_dev/dev_build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eux
+cd $(dirname $0)
+
+docker-compose build
+docker-compose up -d
+docker-compose exec web bash -c 'bash /opt/myapp/src/setup.sh'
+docker-compose restart
--- src/setup.sh
+++ src/setup.sh
@@ -4,4 +4,4 @@
 # npm install -g express-generator
 # express /opt/myapp/src/node --view=ejs
 
-npm isntall -g nodemon
+npm install --prefix /opt/myapp/src/node
