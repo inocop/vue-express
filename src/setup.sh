dac0de5c6539bcd9d25361f4220d02da50b38d9d
inocop <inoue19851110@gmail.com>
2019-02-14 18:40:14 +0900

    refactor docker dir

--- src/setup.sh
+++ src/setup.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+
+########## node ##########
+
 # === already run
 # npm install -g express-generator
 # express /opt/myapp/src/node --view=ejs
 
 npm install --prefix /opt/myapp/src/node
+
+
+
+########## vue ##########
+
+# === already run
+# cd /opt/myapp/src/vue
+# vue create .
+#    Vue CLI v3.4.0
+#    ? Generate project in current directory? (Y/n)
+#    ❯ Y
+#    ? Please pick a preset: (Use arrow keys)
+#    ❯ default (babel, eslint)
+#    Manually select features
+
+npm install --prefix /opt/myapp/src/vue
