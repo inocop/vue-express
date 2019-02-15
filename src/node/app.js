5d1ac1991835267f7ac560a9a6afdacfa8eb3876
inocop <inoue19851110@gmail.com>
2019-02-15 13:14:54 +0900

    move public for node

--- src/node/app.js
+++ src/node/app.js
@@ -22,7 +22,7 @@ app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 /**
  * ルーティング設定
--- src/setup.sh
+++ src/setup.sh
@@ -7,7 +7,9 @@
 # npm install -g express-generator
 # express /opt/myapp/src/node --view=ejs
 
-npm install --prefix /opt/myapp/src/node
+cd /opt/myapp/src/node
+npm install
+npm install --prefix /opt/myapp/src/node/public/static
 
 
 
@@ -16,11 +18,19 @@ npm install --prefix /opt/myapp/src/node
 # === already run
 # cd /opt/myapp/src/vue
 # vue create .
-#    Vue CLI v3.4.0
-#    ? Generate project in current directory? (Y/n)
-#    ❯ Y
-#    ? Please pick a preset: (Use arrow keys)
-#    ❯ default (babel, eslint)
-#    Manually select features
 
-npm install --prefix /opt/myapp/src/vue
+# Vue CLI v3.4.0
+# ? Generate project in current directory? Yes
+#
+# Vue CLI v3.4.0
+# ? Please pick a preset: Manually select features
+# ? Check the features needed for your project: Babel, Router, Vuex, Linter
+# ? Use history mode for router? (Requires proper server setup for index fallback in production) No
+# ? Pick a linter / formatter config: Prettier
+# ? Pick additional lint features: (Press <space> to select, <a> to toggle all, <i> to invert selection)Lint on save
+# ? Where do you prefer placing config for Babel, PostCSS, ESLint, etc.? In dedicated config files
+# ? Save this as a preset for future projects? No
+
+cd /opt/myapp/src/vue
+npm install
+npm run build
