de39161dde906a72010bf802e9d6454823373b52
inocop <inoue19851110@gmail.com>
2019-02-15 20:39:12 +0900

    add nginx

--- src/docker/vue_dev/build.sh
+++ src/docker/vue_dev/build.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)
 
 docker-compose build
 docker-compose up -d
-docker-compose exec web bash -c 'bash /opt/myapp/src/setup.sh'
+docker-compose exec app bash -c 'bash /opt/myapp/src/setup.sh'
 docker-compose restart
--- src/vue/vue.config.js
+++ src/vue/vue.config.js
@@ -8,5 +8,17 @@ module.exports = {
       template: 'public/index.html',
       filename: 'index.html',
     }
+  },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: '8080',
+    public: 'localhost:8989',
+    disableHostCheck: true,
+    watchOptions: {
+      ignored: ['node_modules'],
+      aggregateTimeout: 300,
+      poll: 1000
+    }
   }
 }
