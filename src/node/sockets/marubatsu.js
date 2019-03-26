6b1e30facbbbec674546bde7e7a20abec1db76b3
inocop <inoue19851110@gmail.com>
2019-03-26 16:45:41 +0900

    GameRoomsクラスを作成

--- src/node/sockets/marubatsu.js
+++ src/node/sockets/marubatsu.js
@@ -1,9 +1,10 @@
 
 const Game = require('../models/marubatsu/game');
+const GameRooms = require('../models/marubatsu/GameRooms');
 
 module.exports = function(io) {
 
-  const gameRooms = [];
+  const gameRooms = new GameRooms;
 
   var marubatsu_socket = io.of('/marubatsu/')
   marubatsu_socket.on('connection', (socket) => {
@@ -14,28 +15,28 @@ module.exports = function(io) {
       console.log("CREATE_ROOM");
       console.log(`posted[name:${ data.name }]`)
 
-      // 10部屋以上は作成しない
-      if (gameRooms.length >= 10) {
-        marubatsu_socket.to(socket.id).emit('CREATE_ROOM_RECEIVER', { message: "10部屋以上は作成できません" })
-        return
-      }
-
-      game = new Game(gameRooms.length, data.name)
-      gameRooms[game.id] = game
-
-      marubatsu_socket.emit('CREATE_ROOM_RECEIVER', null, game.params)
+      game = new Game(data.name)
+      gameRooms.addRoom(game)
+      .then(() => {
+        marubatsu_socket.emit('CREATE_ROOM_RECEIVER', null, game.params)
+      })
+      .catch((error) => {
+        if (error instanceof RangeError) {
+          marubatsu_socket.to(socket.id).emit('CREATE_ROOM_RECEIVER', error)
+        }
+      })
     })
 
     // ルーム一覧取得
     socket.on('GET_ROOMS', (callback) => {
       console.log("GET_ROOMS");
-      callback(null, Object.keys(gameRooms).map(id => gameRooms[id].params))
+      callback(null, gameRooms.getRoomNameList())
     })
 
     // マルバツルーム入室
     socket.on('GET_ROOM_DETAIL', (roomId, callback) => {
       console.log("GET_ROOM_DETAIL");
-      targetRoom = gameRooms[roomId]
+      targetRoom = gameRooms.getRoom(roomId)
       if (!targetRoom) return
 
       if (targetRoom.player1 && targetRoom.player2) {
@@ -51,7 +52,7 @@ module.exports = function(io) {
     // マルバツ送信
     socket.on('POST_ROOM_DETAIL', (roomId, data) => {
       console.log("POST_ROOM_DETAIL");
-      targetRoom = gameRooms[roomId]
+      targetRoom = gameRooms.getRoom(roomId)
       if (!targetRoom) return
 
       targetRoom.setPlayData(socket.id, data, () => {
